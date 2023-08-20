a712850d5958ed2c2b4aead396daa893e371b3b7
Jade Forlani-Brennan <jade.forlani.brennan@gmail.com>
2023-08-20 14:11:21 +0200

    :loud_sound: created common logger

---
+++ src/shared/utils.py
@@ -0,0 +1,25 @@
+import logging.config
+import os
+import json
+
+LOGGING_CONFIG_FILE = 'logging.json'
+LOGGER_NAME = 'myapp'
+
+def _build_logging_config_file_path():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    config_file_path = os.path.join(current_dir, '..','..','config', LOGGING_CONFIG_FILE)
+    return (config_file_path)
+
+def setup_logger():
+    config_file_path = _build_logging_config_file_path()
+
+    with open(config_file_path, 'r') as config_file:
+        config = json.load(config_file)
+        logging.config.dictConfig(config)
+    
+    logger = logging.getLogger(LOGGER_NAME)
+    logger.info(f"succesfully setup logger using {config_file_path}")
+    return logger
+
+# Initialize the logger when the module is imported
+logger = setup_logger()
